53b8681fcfbc46fae79cb83e35f45f976e25f153
JuanBogliacino <juanboglia12@gmail.com>
2023-08-18 17:32:00 -0300

    assets create

--- src/data-images.js
+++ src/data-images.js
@@ -5,43 +5,43 @@ const albumes = [
         images: [
             {
                 id: 1,
-                url: '../src/images/animal-1.webp',
+                url: '../src/assets/images/animal-1.webp',
               },
               {
                 id: 2,
-                url: '../src/images/animal-2.jpg',
+                url: '../src/assets/images/animal-2.jpg',
               },
               {
                 id: 3,
-                url: '../src/images/animal-3.jpg',
+                url: '../src/assets/images/animal-3.jpg',
               },
               {
                 id: 4,
-                url: '../src/images/animal-4.jpg',
+                url: '../src/assets/images/animal-4.jpg',
               },
               {
                 id: 5,
-                url: '../src/images/animal-5.jpg',
+                url: '../src/assets/images/animal-5.jpg',
               },
               {
                 id: 6,
-                url: '../src/images/animal-6.jpg',
+                url: '../src/assets/images/animal-6.jpg',
               },
               {
                 id: 7,
-                url: '../src/images/animal-7.jpg',
+                url: '../src/assets/images/animal-7.jpg',
               },
               {
                 id: 8,
-                url: '../src/images/animal-8.jpg',
+                url: '../src/assets/images/animal-8.jpg',
               },
               {
                 id: 9,
-                url: '../src/images/animal-9.webp',
+                url: '../src/assets/images/animal-9.webp',
               },
               {
                 id: 10,
-                url: '../src/images/animal-10.jpg',
+                url: '../src/assets/images/animal-10.jpg',
               }
         ],
       },
@@ -51,43 +51,43 @@ const albumes = [
         images: [
             {
                 id: 11,
-                url: '../src/images/auto-1.jpg',
+                url: '../src/assets/images/auto-1.jpg',
               },
               {
                 id: 12,
-                url: '../src/images/auto-2.jpg',
+                url: '../src/assets/images/auto-2.jpg',
               },
               {
                 id: 13,
-                url: '../src/images/auto-3.jpg',
+                url: '../src/assets/images/auto-3.jpg',
               },
               {
                 id: 14,
-                url: '../src/images/auto-4.webp',
+                url: '../src/assets/images/auto-4.webp',
               },
               {
                 id: 15,
-                url: '../src/images/auto-5.jpg',
+                url: '../src/assets/images/auto-5.jpg',
               },
               {
                 id: 16,
-                url: '../src/images/auto-6.jpg',
+                url: '../src/assets/images/auto-6.jpg',
               },
               {
                 id: 17,
-                url: '../src/images/auto-7.jpg',
+                url: '../src/assets/images/auto-7.jpg',
               },
               {
                 id: 18,
-                url: '../src/images/auto-8.jpg',
+                url: '../src/assets/images/auto-8.jpg',
               },
               {
                 id: 19,
-                url: '../src/images/auto-9.jpg',
+                url: '../src/assets/images/auto-9.jpg',
               },
               {
                 id: 20,
-                url: '../src/images/auto-10.jpg',
+                url: '../src/assets/images/auto-10.jpg',
               },
         ],
       },
@@ -97,43 +97,43 @@ const albumes = [
         images: [
                 {
                 id: 21,
-                url: '../src/images/paisaje-4.jpg',
+                url: '../src/assets/images/paisaje-4.jpg',
                 },
                 {
                   id: 23,
-                  url: '../src/images/paisaje-2.png',
+                  url: '../src/assets/images/paisaje-2.png',
                 },
                 {
                   id: 24,
-                  url: '../src/images/paisaje-3.jpg',
+                  url: '../src/assets/images/paisaje-3.jpg',
                 },
                 {
                     id: 25,
-                    url: '../src/images/paisaje-1.jpg',
+                    url: '../src/assets/images/paisaje-1.jpg',
                 },
                 {
                     id: 26,
-                    url: '../src/images/paisaje-5.jpg',
+                    url: '../src/assets/images/paisaje-5.jpg',
                 },
                 {
                     id: 27,
-                    url: '../src/images/paisaje-6.jpg',
+                    url: '../src/assets/images/paisaje-6.jpg',
                     },
                     {
                       id: 28,
-                      url: '../src/images/paisaje-7.jpg',
+                      url: '../src/assets/images/paisaje-7.jpg',
                     },
                     {
                       id: 29,
-                      url: '../src/images/paisaje-8.jpg',
+                      url: '../src/assets/images/paisaje-8.jpg',
                     },
                     {
                         id: 30,
-                        url: '../src/images/paisaje-9.jpg',
+                        url: '../src/assets/images/paisaje-9.jpg',
                     },
                     {
                         id: 31,
-                        url: '../src/images/paisaje-10.jpg',
+                        url: '../src/assets/images/paisaje-10.jpg',
                     },
         ],
       },
@@ -143,43 +143,43 @@ const albumes = [
         images: [
             {
                 id: 32,
-                url: '../src/images/ficcion-1.jpg',
+                url: '../src/assets/images/ficcion-1.jpg',
               },
               {
                 id: 33,
-                url: '../src/images/ficcion-2.jpg',
+                url: '../src/assets/images/ficcion-2.jpg',
               },
               {
                 id: 34,
-                url: '../src/images/ficcion-3.jpg',
+                url: '../src/assets/images/ficcion-3.jpg',
               },
               {
                 id: 35,
-                url: '../src/images/ficcion-4.jpg',
+                url: '../src/assets/images/ficcion-4.jpg',
               },
               {
                 id: 36,
-                url: '../src/images/ficcion-5.jpg',
+                url: '../src/assets/images/ficcion-5.jpg',
               },
               {
                 id: 37,
-                url: '../src/images/ficcion-6.jpg',
+                url: '../src/assets/images/ficcion-6.jpg',
               },
               {
                 id: 38,
-                url: '../src/images/ficcion-7.jpg',
+                url: '../src/assets/images/ficcion-7.jpg',
               },
               {
                 id: 39,
-                url: '../src/images/ficcion-8.jpg',
+                url: '../src/assets/images/ficcion-8.jpg',
               },
               {
                 id: 40,
-                url: '../src/images/ficcion-9.jpg',
+                url: '../src/assets/images/ficcion-9.jpg',
               },
               {
                 id: 41,
-                url: '../src/images/ficcion-10.jpg',
+                url: '../src/assets/images/ficcion-10.jpg',
               },
         ],
       },
